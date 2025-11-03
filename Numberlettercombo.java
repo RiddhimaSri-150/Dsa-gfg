73cdaa74d3b0b0165a4e17d942044d4e23f3f4df
Codephilia <riddhimashri150@gmail.com>
2025-11-03 23:35:27 +0530

    mew change

--- Numberlettercombo.java
+++ Numberlettercombo.java
@@ -14,4 +14,19 @@ public class Numberlettercombo{
             pad(p + ch, up.substring(1));
         }
     }
+    // Returning output as an ArrayList
+     static ArrayList<String> padRet(String p, String up){
+        if (up.isEmpty()){
+            ArrayList<String> ans = new ArrayList<>();
+            return ans;
+        }
+        int digit = up.charAt(0) - '0';
+        ArrayList<String> ans = new ArrayList<>();
+
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+           ans.addAll( padRet(p + ch, up.substring(1)));
+        }
+        return ans;
+    }
 }
