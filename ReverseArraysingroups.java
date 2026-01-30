8fed5534c3cde30719e5781462c2f992f2179249
Codephilia <riddhimashri150@gmail.com>
2026-01-30 20:16:37 +0530

    new gfg question

---
+++ ReverseArraysingroups.java
@@ -0,0 +1,17 @@
+public class ReverseArraysingroups {
+    public void reverseInGroups(int[] arr, int k){
+        int n = arr.length;
+        for (int i = 0; i < n ; i += k) {
+            int left = i;
+            int right = Math.min(i + k - 1, n - 1);
+
+            int temp =arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+
+        }
+    }
+    
+}
