bc521bb31c1dd88d482ae3565e689c61ccc3d6df
Codephilia <riddhimashri150@gmail.com>
2026-02-17 23:50:05 +0530

    Buy and sell multiple transaction

---
+++ BuyandSellMultiple.java
@@ -0,0 +1,14 @@
+public class BuyandSellMultiple {
+    static int maxProfit(int[] prices) {
+        int res = 0;
+
+        // Keep on adding the difference between
+        // adjacent when the prices a
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                res += prices[i] - prices[i - 1];
+        }
+
+        return res;
+    }
+}
