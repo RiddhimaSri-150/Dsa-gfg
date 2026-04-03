ea0e55512a1d37d220405dd45598491c6d3fd2ec
Codephilia <riddhimashri150@gmail.com>
2026-04-03 19:59:53 +0530

    Shuffle the Array Question

---
+++ ShuffleTheArray.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class ShuffleTheArray {
+    public static int[] shuffle(int[] nums, int n){
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < n; i++) {
+            ans[2 * i] = nums[i];
+            ans[2 * i + 1] = nums[i + n];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,5,1,3,4,7};
+        int[] result = shuffle(arr, 3);
+        System.out.println(Arrays.toString(result));
+    }
+}
