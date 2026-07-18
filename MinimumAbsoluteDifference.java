5fd7a4a12d1398718793300206c9b8a7cee2beb3
Codephilia <riddhimashri150@gmail.com>
2026-07-18 23:24:37 +0530

    Minimum Absolute Difference

---
+++ MinimumAbsoluteDifference.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+public class MinimumAbsoluteDifference {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+
+        int minDiff = Integer.MAX_VALUE;
+        List<List<Integer>> result = new ArrayList<>();
+
+        // Step 1: Find minimum difference
+        for (int i = 1; i < arr.length; i++) {
+            minDiff = Math.min(minDiff, arr[i] - arr[i - 1]);
+        }
+
+        // Step 2: Collect pairs
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] == minDiff) {
+                result.add(Arrays.asList(arr[i - 1], arr[i]));
+            }
+        }
+
+        return result;
+    }
+}
