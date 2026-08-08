1f847269b8eb265c2c46184e3a6d5f88a45316d4
Codephilia <riddhimashri150@gmail.com>
2026-08-08 23:48:21 +0530

    3

--- ReverseStringIII.java
+++ ReverseStringIII.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class ReverseStringIII {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (String word : words) {
+            StringBuilder reversedWord = new StringBuilder(word).reverse();
+            result.append(reversedWord).append(" ");
+        }
+        result.deleteCharAt(result.length() - 1);  // Remove the extra space at the end
+
+        // Convert StringBuilder to String
+        return result.toString();
+    }
 }
